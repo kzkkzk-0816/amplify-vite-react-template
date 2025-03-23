58c1937673340d393e38849b3be7ede42805ac0f
kzkkzk-0816 <kzkkzk0816@gmail.com>
2025-03-23 15:15:03 +0000

    add storage backend 3

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,8 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
-defineBackend({
+
+const backend = defineBackend({
   auth,
   data,
   storage,
